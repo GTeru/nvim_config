58ac275e899c4000fcb1eee7ba8c75c3f4e2b164
GTeru <gmteruya@hotmail.com>
2025-11-21 18:52:11 -0300

    init config

---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    dependencies = {
+      "LazyVim/LazyVim",
+    },
+    opts = {
+      flavour = "mocha",
+    },
+  },
+}
